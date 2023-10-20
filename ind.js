9d3037ab1efde294c0488e6ef26e9f0b08804ca5
Sriram5-coder <saisriramkollepara@gmail.com>
2023-10-20 21:38:19 +0530

    travi tourism website

---
+++ ind.js
@@ -0,0 +1,68 @@
+function myFunction() {
+  var x = document.getElementById("obob");
+  var y=document.getElementById("b1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display="none";
+    setTimeout(myFunction,4000);
+  } else {
+    x.style.display = "none";
+    y.style.display="block";
+  }
+}
+function myFunction1() {
+  var x = document.getElementById("obob1");
+  var y=document.getElementById("b2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display="none";
+    setTimeout(myFunction1,4000);
+  } else {
+    x.style.display = "none";
+    y.style.display="block";
+  }
+}function myFunction2() {
+  var x = document.getElementById("obob2");
+  var y=document.getElementById("b3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display="none";
+    setTimeout(myFunction2,4000);
+  } else {
+    x.style.display = "none";
+    y.style.display="block";
+  }
+}function myFunction3() {
+  var x = document.getElementById("obob3");
+  var y=document.getElementById("b4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display="none";
+    setTimeout(myFunction3,4000);
+  } else {
+    x.style.display = "none";
+    y.style.display="block";
+  }
+}function myFunction4() {
+  var x = document.getElementById("obob4");
+  var y=document.getElementById("b5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display="none";
+    setTimeout(myFunction4,4000);
+  } else {
+    x.style.display = "none";
+    y.style.display="block";
+  }
+}function myFunction5() {
+  var x = document.getElementById("obob5");
+  var y=document.getElementById("b6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display="none";
+    setTimeout(myFunction5,4000);
+  } else {
+    x.style.display = "none";
+    y.style.display="block";
+  }
+}
